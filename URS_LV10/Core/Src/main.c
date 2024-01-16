43b55fd3f109eaaa253344cca357d2ed564247cf
„Borna <bornacrnkovic@gmail.com>
2024-01-16 17:10:19 +0100

    dodan blinky program

--- URS_LV10/Core/Src/main.c
+++ URS_LV10/Core/Src/main.c
@@ -63,6 +63,16 @@ void SystemClock_Config(void);
   */
 int main(void)
 {
+	HAL_Init();
+	 SystemClock_Config();
+	 MX_GPIO_Init();
+	 while (1){
+	 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+	 HAL_Delay(200);
+	 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+	 HAL_Delay(200);
+	 }
+
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
